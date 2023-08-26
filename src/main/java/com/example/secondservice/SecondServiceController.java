03ff34806ecd630e6e20b7c15af25c896f6d30c4
isckd <isckd3@gmail.com>
2023-08-27 00:26:01 +0900

    apigateway testgateway .yml -> java code routing header test

--- src/main/java/com/example/secondservice/SecondServiceController.java
+++ src/main/java/com/example/secondservice/SecondServiceController.java
@@ -1,6 +1,7 @@
 package com.example.secondservice;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +9,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/second-service")
 public class SecondServiceController {
 
+    private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(SecondServiceController.class);
+
     @GetMapping("/welcome")
     public String welcome() {
         return "Welcome to the Second Service";
+    }
+
+    @GetMapping
+    public String message(@RequestHeader("second-request") String header) {
+        log.info(header);
+        return "Hello World in Second Service";
     }
 }
